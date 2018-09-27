ef46e72bd299f0ec64fb2e803bc24201661e799c
SP_Penguin <abrcdf1023@gmail.com>
2018-09-27 15:02:25 +0800

    add some comment

--- src/createFetchPostReducer.d.ts
+++ src/createFetchPostReducer.d.ts
@@ -1,6 +1,14 @@
 import { fromJS } from 'immutable'
 import { handleActions } from 'redux-actions'
 
+/**
+ * success config option
+ * example: [FETCH_GET_SUCCESS, {
+ *  mergeData: true,
+ * }]
+ * options: mergeData, mergeDeepData
+ * To figure out what's different please read immutablejs documentation
+ */
 export interface Config {
   take: string;
   request: string;
