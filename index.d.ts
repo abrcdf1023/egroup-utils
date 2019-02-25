350f7946bdc1621e67dc823e6f540e3958da7501
SP_Penguin <abrcdf1023@gmail.com>
2019-02-25 14:11:15 +0800

    fixed export bug

--- index.d.ts
+++
@@ -1,3 +0,0 @@
-export { default as createFetchReducer } from './src/createFetchReducer'
-export { default as createObservableApi } from './src/createObservableApi'
-export { default as initialState } from './src/initialState'
