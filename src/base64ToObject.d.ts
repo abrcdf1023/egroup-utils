3724bd4ac86254d1e199c34f1601ea15538889cf
SP_Penguin <abrcdf1023@gmail.com>
2019-03-05 11:53:19 +0800

    feat add new base64ToObject function

---
+++ src/base64ToObject.d.ts
@@ -0,0 +1,3 @@
+export default function base64ToObject(
+  b64String: String
+): void;
---
+++ src/base64ToObject.js
@@ -0,0 +1,15 @@
+import { Base64 } from 'js-base64';
+
+/**
+ * A function parse base64 string to javascript object.
+ * @param {*} b64String 
+ */
+export default function base64ToObject(b64String) {
+  let value = {};
+  try {
+    value = JSON.parse(Base64.decode(b64String));
+  } catch (error) {
+    console.log(error);
+  }
+  return value;
+}
