ba383b2ba213d19d59da150e67622d28cd8acdfb
SP_Penguin <abrcdf1023@gmail.com>
2019-04-03 12:19:58 +0800

    fixed(createGulpTasks) cjs import bug

--- src/createGulpTasks.js
+++ src/createGulpTasks.js
@@ -1,9 +1,9 @@
-import path from "path";
-import gulp from "gulp";
-import plumber from "gulp-plumber";
-import bom from "gulp-bom";
-import rename from "gulp-rename";
-import clean from "gulp-clean";
+const path = require("path");
+const gulp = require("gulp");
+const plumber = require("gulp-plumber");
+const bom = require("gulp-bom");
+const rename = require("gulp-rename");
+const clean = require("gulp-clean");
 
 export default function createGulpTasks({ serverDir, buildJsp, buildFolder }) {
   const jspName = path.basename(buildJsp);
