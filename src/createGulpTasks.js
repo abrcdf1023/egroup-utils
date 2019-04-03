159e3e4ad404165cd4c30af56646ef215c539ec0
SP_Penguin <abrcdf1023@gmail.com>
2019-04-03 11:44:22 +0800

    format file

--- src/createGulpTasks.js
+++ src/createGulpTasks.js
@@ -1,31 +1,27 @@
-function createGulpTasks({
-  serverDir,
-  buildJsp,
-  buildFolder
-}) {
-  const path = require('path')
-  const gulp = require('gulp');
-  const plumber = require('gulp-plumber');
-  const bom = require('gulp-bom');
-  const rename = require('gulp-rename');
-  const clean = require('gulp-clean');
+function createGulpTasks({ serverDir, buildJsp, buildFolder }) {
+  const path = require("path");
+  const gulp = require("gulp");
+  const plumber = require("gulp-plumber");
+  const bom = require("gulp-bom");
+  const rename = require("gulp-rename");
+  const clean = require("gulp-clean");
 
-  const jspName = path.basename(buildJsp)
-  const jspDir = path.dirname(buildJsp)
+  const jspName = path.basename(buildJsp);
+  const jspDir = path.dirname(buildJsp);
 
   /**
    * html to jsp
    * We don't need to clean jsp here because it'll clean at landing page build process.
    * Therefore we need run build landing page first.
    */
-  gulp.task('toJSP', () =>
+  gulp.task("toJSP", () =>
     gulp
-      .src('build/index.html')
+      .src("build/index.html")
       .pipe(
         plumber({
           errorHandler(error) {
             console.log(error.message);
-            this.emit('end');
+            this.emit("end");
           }
         })
       )
@@ -38,7 +34,7 @@ function createGulpTasks({
   /**
    * clean build folder
    */
-  gulp.task('cleanBuild', () =>
+  gulp.task("cleanBuild", () =>
     gulp
       .src(`${serverDir}${buildFolder}`, {
         read: false,
@@ -54,17 +50,16 @@ function createGulpTasks({
   /**
    * copy build to backend
    */
-  gulp.task('copyBuild', () =>
+  gulp.task("copyBuild", () =>
     gulp
-      .src(['build/**/*', '!build/index.html'])
+      .src(["build/**/*", "!build/index.html"])
       .pipe(gulp.dest(`${serverDir}${buildFolder}`))
   );
 
   /**
    * production
    */
-  gulp.task('production', gulp.series(['toJSP', 'cleanBuild', 'copyBuild']));
-
+  gulp.task("production", gulp.series(["toJSP", "cleanBuild", "copyBuild"]));
 }
 
-module.exports = createGulpTasks
+module.exports = createGulpTasks;
