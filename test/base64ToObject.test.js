7f272eca754049886d2b48a1a19e28af6601c802
SP_Penguin <abrcdf1023@gmail.com>
2019-04-20 18:30:20 +0800

    format base64ToObject.test.js

--- test/base64ToObject.test.js
+++ test/base64ToObject.test.js
@@ -1,8 +1,8 @@
-import { Base64 } from 'js-base64'
-import { base64ToObject } from '../src'
+import { Base64 } from "js-base64";
+import { base64ToObject } from "../src";
 
-const source = { foo: "bar" }
-const base64 = Base64.encode(JSON.stringify(source))
+const source = { foo: "bar" };
+const base64 = Base64.encode(JSON.stringify(source));
 
 test("base64 object to js object", () => {
   expect(base64ToObject(base64)).toEqual(source);
