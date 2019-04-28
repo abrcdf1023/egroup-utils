9491195193d46891294c0619d08bba3b12d2f0f7
SP_Penguin <abrcdf1023@gmail.com>
2019-04-28 17:42:29 +0800

    [chore] fixed file structure

--- src/createFetchReducer.js
+++ src/createFetchReducer.js
@@ -1,3 +1,6 @@
+import { handleActions } from "redux-actions";
+import { Map } from "immutable";
+
 /**
  * @private
  * @param {*} success
@@ -43,13 +46,7 @@ function makeCancelAction(cancel) {
  * @param {Object|null} config
  * @return {Object}
  */
-export default function makeFetchActions({
-  take,
-  request,
-  success,
-  cancel,
-  failure
-}) {
+function makeFetchActions({ take, request, success, cancel, failure }) {
   return {
     [take]: state => state.set("isError", false),
     [request]: state => state.set("isLoading", true),
@@ -64,4 +61,52 @@ export default function makeFetchActions({
       return newState;
     }
   };
+}
+
+/**
+ * @private
+ * @param {*} config
+ */
+function checkConfig(config) {
+  if (!config) {
+    throw new Error("Config is required but it's undefined.");
+  } else {
+    const { take, request, success, failure } = config;
+    if (!take) throw new Error("Config take is required but it's undefined.");
+    if (!request)
+      throw new Error("Config request is required but it's undefined.");
+    if (!success)
+      throw new Error("Config success is required but it's undefined.");
+    if (!failure)
+      throw new Error("Config failure is required but it's undefined.");
+  }
+}
+
+/**
+ * create fetch reducer by config,
+ * First arg is the config
+ * Second arg can replace the initialState you need
+ * Third arg can replace the action function you need
+ * @param {Object} config
+ * @param {Object|null} cusInitialState
+ * @param {Object|null} cusActions
+ */
+export default function createFetchReducer(
+  config,
+  cusInitialState,
+  cusActions
+) {
+  checkConfig(config);
+  const actions = makeFetchActions(config);
+  const initialState = Map({
+    isLoading: false,
+    isError: false
+  });
+  return handleActions(
+    {
+      ...actions,
+      ...cusActions
+    },
+    initialState.merge(cusInitialState)
+  );
 }
