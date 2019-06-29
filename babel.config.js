80c05f6b7b939ce8704e4c467be65334a6f2a6a7
SP_Penguin <abrcdf1023@gmail.com>
2019-06-29 13:12:46 +0800

    [chore] fixed babel config

--- babel.config.js
+++ babel.config.js
@@ -1,31 +1,24 @@
 module.exports = function (api) {
-  const isTest = api.env(["test"])
-  const presets = [
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          browsers: ["ie >= 11"],
-          node: 'current',
-        },
-        exclude: ["transform-async-to-generator", "transform-regenerator"],
-        // use ES6 module
-        modules: isTest ? undefined : false,
-        // loose模式說明 https://www.w3ctech.com/topic/1708
-        loose: true
-      },
-    ],
-  ]
-  
-  const plugins = [
-    "@babel/plugin-proposal-object-rest-spread"
-  ]
+  if (api.env(["test"])) {
+    return {
+      presets: ['@babel/preset-env']
+    }
+  }
 
   return {
-    presets,
-    plugins,
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          // for browserslist in package.json
+          useBuiltIns: 'entry',
+          // use ES6 module
+          modules: false
+        },
+      ],
+    ],
     ignore: [
       "**/*.test.js"
     ]
-  };
+  }
 }
