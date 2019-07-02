35a7ac119b5bcd995834121b36c8c7e6028cc433
SP_Penguin <abrcdf1023@gmail.com>
2019-07-02 11:55:12 +0800

    [hooks] update useInfiniteScroll

--- packages/hooks/src/useInfiniteScroll.js
+++ packages/hooks/src/useInfiniteScroll.js
@@ -1,19 +1,25 @@
 import React from 'react';
 
-export default function useInfiniteScroll(value) {
-  const [page, setPage] = React.useState(value);
+export default function useInfiniteScroll({
+  defaultValue,
+  isLoading,
+  maxPage
+}) {
+  const [page, setPage] = React.useState(defaultValue || 0);
 
   React.useEffect(() => {
     const handleScroll = () => {
+      if (page + 1 > maxPage) return;
+      if (isLoading) return;
       if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
-        setPage(page + 1);
+        setPage(value => value + 1);
       }
     };
     window.addEventListener('scroll', handleScroll);
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
-  }, [page]);
+  }, [isLoading, maxPage, page]);
 
-  return page;
+  return [page, setPage];
 }
