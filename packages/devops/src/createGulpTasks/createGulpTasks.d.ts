a690aaf40bd7367f91f7c5c00a06f8b05a183ed7
SP_Penguin <abrcdf1023@gmail.com>
2019-07-03 16:19:07 +0800

    [devops] createGulpTasks make buildJsp optional

--- packages/devops/src/createGulpTasks/createGulpTasks.d.ts
+++ packages/devops/src/createGulpTasks/createGulpTasks.d.ts
@@ -1,7 +1,7 @@
 interface CreateGulpTasksConfig {
   serverDir: string;
-  buildJsp: string;
   buildFolder: string;
+  buildJsp?: string;
 }
 
 export default function createGulpTasks(config: CreateGulpTasksConfig): void;
--- packages/devops/src/createGulpTasks/createGulpTasks.js
+++ packages/devops/src/createGulpTasks/createGulpTasks.js
@@ -1,40 +1,40 @@
-const path = require("path");
-const gulp = require("gulp");
-const plumber = require("gulp-plumber");
-const bom = require("gulp-bom");
-const rename = require("gulp-rename");
-const clean = require("gulp-clean");
+const path = require('path');
+const gulp = require('gulp');
+const plumber = require('gulp-plumber');
+const bom = require('gulp-bom');
+const rename = require('gulp-rename');
+const clean = require('gulp-clean');
 
-function createGulpTasks({ serverDir, buildJsp, buildFolder }) {
-  const jspName = path.basename(buildJsp);
-  const jspDir = path.dirname(buildJsp);
-
+function createGulpTasks({ serverDir, buildFolder, buildJsp }) {
   /**
    * html to jsp
-   * We don't need to clean jsp here because it'll clean at landing page build process.
-   * Therefore we need run build landing page first.
    */
-  gulp.task("toJSP", () =>
-    gulp
-      .src("build/index.html")
-      .pipe(
-        plumber({
-          errorHandler(error) {
-            console.log(error.message);
-            this.emit("end");
-          }
-        })
-      )
-      // gulp bom is to conver files to utf-8
-      .pipe(bom())
-      .pipe(rename(jspName))
-      .pipe(gulp.dest(`${serverDir}${jspDir}`))
-  );
+  gulp.task('toJSP', () => {
+    if (!buildJsp) return Promise.resolve('done');
+    const jspName = path.basename(buildJsp);
+    const jspDir = path.dirname(buildJsp);
+    return (
+      gulp
+        .src('build/index.html')
+        .pipe(
+          plumber({
+            errorHandler(error) {
+              console.log(error.message);
+              this.emit('end');
+            }
+          })
+        )
+        // gulp bom is to conver files to utf-8
+        .pipe(bom())
+        .pipe(rename(jspName))
+        .pipe(gulp.dest(`${serverDir}${jspDir}`))
+    );
+  });
 
   /**
    * clean build folder
    */
-  gulp.task("cleanBuild", () =>
+  gulp.task('cleanBuild', () =>
     gulp
       .src(`${serverDir}${buildFolder}`, {
         read: false,
@@ -48,18 +48,23 @@ function createGulpTasks({ serverDir, buildJsp, buildFolder }) {
   );
 
   /**
-   * copy build to backend
+   * copy build to serverDir
    */
-  gulp.task("copyBuild", () =>
-    gulp
-      .src(["build/**/*", "!build/index.html"])
-      .pipe(gulp.dest(`${serverDir}${buildFolder}`))
-  );
+  gulp.task('copyBuild', () => {
+    if (!buildJsp) {
+      return gulp
+        .src('build/**/*')
+        .pipe(gulp.dest(`${serverDir}${buildFolder}`));
+    }
+    return gulp
+      .src(['build/**/*', '!build/index.html'])
+      .pipe(gulp.dest(`${serverDir}${buildFolder}`));
+  });
 
   /**
    * production
    */
-  gulp.task("production", gulp.series(["toJSP", "cleanBuild", "copyBuild"]));
+  gulp.task('production', gulp.series(['toJSP', 'cleanBuild', 'copyBuild']));
 }
 
 module.exports = createGulpTasks;
