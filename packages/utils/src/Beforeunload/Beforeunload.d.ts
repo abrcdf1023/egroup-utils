a0fa5d0a0333667bb2710c27667f50203c08cab3
SP_Penguin <abrcdf1023@gmail.com>
2020-03-13 12:02:42 +0800

    [utils] feat Beforeunload definition

---
+++ packages/utils/src/Beforeunload/Beforeunload.d.ts
@@ -0,0 +1,24 @@
+/*~ Write your module's methods and properties in this class */
+declare class Beforeunload {
+  register(options: Beforeunload.RegisterOptions): void;
+  block(): void;
+  unblock(): void;
+}
+
+/*~ If you want to expose types from your module as well, you can
+ *~ place them in this block.
+ *~
+ *~ Note that if you decide to include this namespace, the module can be
+ *~ incorrectly imported as a namespace object, unless
+ *~ --esModuleInterop is turned on:
+ *~   import * as x from '[~THE MODULE~]'; // WRONG! DO NOT DO THIS!
+ */
+declare namespace Beforeunload {
+  export interface RegisterOptions {
+    onbeforeunload?: () => void;
+  }
+}
+
+const Instance = new Beforeunload();
+
+export default Instance;
