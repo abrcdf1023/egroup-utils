4077364b88de3b94bb047bc1c6b83d215e7dddaf
SP_Penguin <abrcdf1023@gmail.com>
2020-04-02 19:51:12 +0800

    [utils] reduxFormUtils done

--- packages/utils/src/reduxFormUtils/reduxFormUtils.js
+++ packages/utils/src/reduxFormUtils/reduxFormUtils.js
@@ -1,51 +1,90 @@
 import { fromJS, List, isImmutable, Map } from 'immutable';
 
 /**
+ * If formatter value is string or boolean get the correct option format for react select.
+ */
+function getFormattedSelectOption({
+  value,
+  labelPath,
+  valuePath,
+  selectOptions
+}) {
+  if (typeof value === 'string' || typeof value === 'number') {
+    if (selectOptions) {
+      const selectedOption = selectOptions.filter(
+        el => el[valuePath] === value
+      )[0];
+      if (selectedOption) {
+        return fromJS(selectedOption);
+      }
+    }
+    return fromJS({
+      label: value,
+      value
+    });
+  }
+  if (Map.isMap(value)) {
+    return fromJS({
+      ...value.toJS(),
+      label: value.getIn(labelPath),
+      value: value.getIn(valuePath)
+    });
+  }
+  return value;
+}
+
+/**
  * Make react select redux form formatter.
  * @param {*} options
  * @param {array} options.labelPath
  * @param {array} options.valuePath
  */
-export function makeReactSelectFormatter(options) {
-  const { labelPath = ['label'], valuePath = ['value'] } = options;
+export function makeReactSelectFormatter(options = {}) {
+  const {
+    labelPath = ['label'],
+    valuePath = ['value'],
+    options: selectOptions
+  } = options;
   return function formatter(value, name) {
-    if (typeof value === 'string' || typeof value === 'number') {
-      return fromJS({
-        label: value,
-        value
-      });
-    }
-    if (Map.isMap(value)) {
-      return fromJS({
-        label: value.getIn(labelPath),
-        value: value.getIn(valuePath)
-      });
-    }
     if (List.isList(value)) {
-      return value.map(el => {
-        if (typeof el === 'string' || typeof el === 'number') {
-          return fromJS({
-            label: el,
-            value: el
-          });
-        }
-        return fromJS({
-          label: el.getIn(labelPath),
-          value: el.getIn(valuePath)
-        });
-      });
+      return value.map(el =>
+        getFormattedSelectOption({
+          value: el,
+          labelPath,
+          valuePath,
+          selectOptions
+        })
+      );
     }
-    return value;
+    return getFormattedSelectOption({
+      value,
+      labelPath,
+      valuePath,
+      selectOptions
+    });
   };
+}
+
+/**
+ * If react select options value is immutable Map get the correct value format to store in redux form.
+ */
+function getNormalizedReduxFormValue({ value, disableReturnStringValue }) {
+  if (Map.isMap(value)) {
+    if (disableReturnStringValue) {
+      return value.deleteAll(['label', 'value']);
+    }
+    return value.get('value');
+  }
+  return value;
 }
 
 /**
  * Make react select redux form normalizer.
  * @param {*} options
- * @param {array} options.valuePath
+ * @param {boolean} options.disableReturnStringValue set `true` to disable return String value
  */
-export function makeReactSelectNormalizer(options) {
-  const { valuePath = ['value'] } = options;
+export function makeReactSelectNormalizer(options = {}) {
+  const { disableReturnStringValue } = options;
   return function normalizer(
     value,
     previousValue,
@@ -54,13 +93,34 @@ export function makeReactSelectNormalizer(options) {
     name
   ) {
     if (isImmutable(value)) {
-      if (Map.isMap(value)) {
-        return value.getIn(valuePath);
-      }
       if (List.isList(value)) {
-        return value.map(el => el.getIn(valuePath));
+        return value.map(el =>
+          getNormalizedReduxFormValue({
+            value: el,
+            disableReturnStringValue
+          })
+        );
       }
+      return getNormalizedReduxFormValue({
+        value,
+        disableReturnStringValue
+      });
     }
     return value;
   };
+}
+
+/**
+ * Format select options for react select
+ * @param {*} options
+ * @param {array} options.labelPath
+ * @param {array} options.valuePath
+ */
+export function formatSelectOptions(options = {}) {
+  const { labelPath = [], valuePath = [], options: selectOptions } = options;
+  return selectOptions.map(el => ({
+    ...el,
+    label: fromJS(el).getIn(labelPath),
+    value: fromJS(el).getIn(valuePath)
+  }));
 }
