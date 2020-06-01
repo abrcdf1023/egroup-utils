129f1b885158b2fb01ec37ddff59689c1c4cc289
SP_Penguin <abrcdf1023@gmail.com>
2020-06-01 19:54:25 +0800

    [utils] update isTwId

--- packages/utils/src/isTwId/isTwId.js
+++ packages/utils/src/isTwId/isTwId.js
@@ -2,44 +2,93 @@ const isString = id => typeof id === 'string';
 
 const isLengthValid = id => id.length === 10;
 
-const isFormatValid = id => /^[A-Z][12][0-9]{8}$/.test(id);
+const isTwIdFormatValid = id => /^[A-Z][12][0-9]{8}$/.test(id);
+
+const isResidentPermitIdFormatValid = id => /^[A-Z][ABCD][0-9]{8}$/.test(id);
 
 /**
- * 身分證字號規則 http://web.fg.tp.edu.tw/~anny/idtest.htm
+ * 把對應的英文字母轉換成二位數字
+ */
+const parseLetterToNumber = letter => {
+  const letters = 'ABCDEFGHJKLMNPQRSTUVXYWZIO';
+  return letters.indexOf(letter) + 10;
+};
+
+/**
+ * 取得第一個字母的加權值
  * @param {String} id
  */
-const isTwIdValid = id => {
+const getFirstValue = letter => {
   // 第一碼英文字母轉換為二位數字碼
-  const letters = 'ABCDEFGHJKLMNPQRSTUVXYWZIO';
-  const firstLetterNumber = letters.indexOf(id[0]) + 10;
+  const firstNumber = parseLetterToNumber(letter);
   // 轉換型態方便待會拆數字
-  const firstLetterNumberString = String(firstLetterNumber);
+  const firstNumberString = String(firstNumber);
   // 第一個字母的加權值
-  const firstLetterValue =
-    Number(firstLetterNumberString.charAt(0)) * 1 +
-    Number(firstLetterNumberString.charAt(1)) * 9;
-  // 準備計算後面 8 碼(扣除驗證碼)的加權值
-  let idTailSum = 0;
-  const idTail = id.slice(1, 9);
+  return (
+    Number(firstNumberString.charAt(0)) * 1 +
+    Number(firstNumberString.charAt(1)) * 9
+  );
+};
+
+/**
+ * 取得後面 8 碼(扣除驗證碼)的加權值
+ * @param {String} id
+ */
+const getSum = others => {
+  let sum = 0;
   // 權重由 8 開始遞減至 1
   let weight = 8;
-  for (let i = 0; i < idTail.length; i++) {
-    const number = Number(idTail[i]);
-    idTailSum += number * weight;
+  for (let i = 0; i < others.length; i++) {
+    const number = Number(others[i]);
+    sum += number * weight;
     weight--;
   }
-  // 驗證檢查碼是否正確
-  const checkNumber = Number(id.slice(-1));
-  const remainder = (firstLetterValue + idTailSum) % 10;
-  // 由模數減去餘數得檢查號碼，若餘數為 0 時，則設定其檢查碼為 0
+  return sum;
+};
+
+/**
+ * 驗證檢查碼是否正確
+ * 由模數減去餘數得檢查號碼，若餘數為 0 時，則設定其檢查碼為 0
+ * @param {Number} remainder
+ * @param {Number} checkNumber
+ */
+const checkNumberIsValid = (remainder, checkNumber) => {
   if (remainder === 0) {
     return checkNumber === 0;
   }
   return checkNumber === 10 - remainder;
 };
 
+/**
+ * 身分證字號規則 http://web.fg.tp.edu.tw/~anny/idtest.htm
+ * @param {String} id
+ */
+const isTwIdValid = id => {
+  if (!isTwIdFormatValid(id)) return false;
+  const firstValue = getFirstValue(id[0]);
+  const idTailSum = getSum(id.slice(1, 9));
+  const remainder = (firstValue + idTailSum) % 10;
+  const checkNumber = Number(id.slice(-1));
+  return checkNumberIsValid(remainder, checkNumber);
+};
+
+/**
+ * 居留證號碼規則 http://web.fg.tp.edu.tw/~anny/idtest.htm
+ * @param {String} id
+ */
+const isResidentPermitIdValid = id => {
+  if (!isResidentPermitIdFormatValid(id)) return false;
+  const firstValue = getFirstValue(id[0]);
+  const secondNumberString = String(parseLetterToNumber(id[1]));
+  const secondNumber = Number(secondNumberString.charAt(1));
+  const idTailSum = getSum([secondNumber, ...id.slice(2, 9)]);
+  const remainder = (firstValue + idTailSum) % 10;
+  const checkNumber = Number(id.slice(-1));
+  return checkNumberIsValid(remainder, checkNumber);
+};
+
 export default function isTwId(id) {
-  return (
-    isString(id) && isLengthValid(id) && isFormatValid(id) && isTwIdValid(id)
-  );
+  if (!isString(id)) return false;
+  if (!isLengthValid(id)) return false;
+  return isTwIdValid(id) || isResidentPermitIdValid(id);
 }
--- packages/utils/src/isTwId/isTwId.test.js
+++ packages/utils/src/isTwId/isTwId.test.js
@@ -1,23 +1,47 @@
 import isTwId from './isTwId';
 
 describe('isTwId', () => {
-  it('should pass', () => {
+  it('should pass with TW Id', () => {
     expect(isTwId('A123456789')).toEqual(true);
   });
 
-  it('should pass', () => {
+  it('should pass with TW Id', () => {
     expect(isTwId('B134863148')).toEqual(true);
   });
 
-  it('should pass', () => {
+  it('should pass with TW Id', () => {
     expect(isTwId('C193753053')).toEqual(true);
   });
 
-  it('should pass', () => {
+  it('should pass with TW Id', () => {
     expect(isTwId('D172455240')).toEqual(true);
   });
 
-  it('should not pass', () => {
+  it('should not pass with TW Id', () => {
     expect(isTwId('A987654321')).toEqual(false);
+  });
+
+  it('should pass with TW resident permit Id', () => {
+    expect(isTwId('AC82420552')).toEqual(true);
+  });
+
+  it('should pass with TW resident permit Id', () => {
+    expect(isTwId('AD34096702')).toEqual(true);
+  });
+
+  it('should pass with TW resident permit Id', () => {
+    expect(isTwId('AA14250777')).toEqual(true);
+  });
+
+  it('should pass with TW resident permit Id', () => {
+    expect(isTwId('AB64839220')).toEqual(true);
+  });
+
+  it('should not pass with TW resident permit Id', () => {
+    expect(isTwId('AE87654321')).toEqual(false);
+  });
+
+  it('should not pass with TW resident permit Id', () => {
+    expect(isTwId('AA12345678')).toEqual(false);
   });
 });
