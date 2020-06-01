494215baed32e0b101b9f3552fd85c343d167df5
SP_Penguin <abrcdf1023@gmail.com>
2020-06-01 19:35:44 +0800

    [utils] update isTwId

--- packages/utils/src/isTwId/isTwId.js
+++ packages/utils/src/isTwId/isTwId.js
@@ -4,31 +4,42 @@ const isLengthValid = id => id.length === 10;
 
 const isFormatValid = id => /^[A-Z][12][0-9]{8}$/.test(id);
 
-const isChecksumValid = id => {
-  const IDLEN = 10;
-  // each letter represents value from [10..35]
+/**
+ * 身分證字號規則 http://web.fg.tp.edu.tw/~anny/idtest.htm
+ * @param {String} id
+ */
+const isTwIdValid = id => {
+  // 第一碼英文字母轉換為二位數字碼
   const letters = 'ABCDEFGHJKLMNPQRSTUVXYWZIO';
-  const letterIndex = letters.indexOf(id[0]) + 10;
-  const letterValue =
-    Math.floor(letterIndex / 10) + (letterIndex % 10) * (IDLEN - 1);
-  const idTail = id.slice(1); // drop the letter
-  let weight = IDLEN - 2; // minus letter digit and check digit
-  let weightedSum = 0;
+  const firstLetterNumber = letters.indexOf(id[0]) + 10;
+  // 轉換型態方便待會拆數字
+  const firstLetterNumberString = String(firstLetterNumber);
+  // 第一個字母的加權值
+  const firstLetterValue =
+    Number(firstLetterNumberString.charAt(0)) * 1 +
+    Number(firstLetterNumberString.charAt(1)) * 9;
+  // 準備計算後面 8 碼(扣除驗證碼)的加權值
+  let idTailSum = 0;
+  const idTail = id.slice(1, 9);
+  // 權重由 8 開始遞減至 1
+  let weight = 8;
   for (let i = 0; i < idTail.length; i++) {
-    const char = idTail[i];
-    weightedSum += +char * weight;
+    const number = Number(idTail[i]);
+    idTailSum += number * weight;
     weight--;
   }
-  // note: the check digit of 'id' is weighted 0
-  const remainder = (letterValue + weightedSum + +id.slice(-1)) % 10;
-  return remainder === 0;
+  // 驗證檢查碼是否正確
+  const checkNumber = Number(id.slice(-1));
+  const remainder = (firstLetterValue + idTailSum) % 10;
+  // 由模數減去餘數得檢查號碼，若餘數為 0 時，則設定其檢查碼為 0
+  if (remainder === 0) {
+    return checkNumber === 0;
+  }
+  return checkNumber === 10 - remainder;
 };
 
 export default function isTwId(id) {
   return (
-    isString(id) &&
-    isLengthValid(id) &&
-    isFormatValid(id) &&
-    isChecksumValid(id)
+    isString(id) && isLengthValid(id) && isFormatValid(id) && isTwIdValid(id)
   );
 }
--- packages/utils/src/isTwId/isTwId.test.js
+++ packages/utils/src/isTwId/isTwId.test.js
@@ -5,6 +5,18 @@ describe('isTwId', () => {
     expect(isTwId('A123456789')).toEqual(true);
   });
 
+  it('should pass', () => {
+    expect(isTwId('B134863148')).toEqual(true);
+  });
+
+  it('should pass', () => {
+    expect(isTwId('C193753053')).toEqual(true);
+  });
+
+  it('should pass', () => {
+    expect(isTwId('D172455240')).toEqual(true);
+  });
+
   it('should not pass', () => {
     expect(isTwId('A987654321')).toEqual(false);
   });
