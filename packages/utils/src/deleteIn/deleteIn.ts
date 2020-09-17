7457534b3a3e8aea637d4f5201b4cd2ca8326736
SP_Penguin <abrcdf1023@gmail.com>
2020-09-17 15:56:15 +0800

    [utils] refactor deleteIn to ts

--- packages/utils/src/deleteIn/deleteIn.ts
+++ packages/utils/src/deleteIn/deleteIn.ts
@@ -3,7 +3,7 @@
  * @param {any} obj
  * @param {Array<String>} paths
  */
-export default function deleteIn(obj, paths) {
+export default function deleteIn(obj: any, paths: (string | number)[]) {
   if (!obj) return;
   let copy = obj;
   for (let i = 0; i < paths.length; i++) {
